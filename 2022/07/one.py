9e5c67b3bd5ea90df170008eb19535d0fb09a509
Rob Lass <rob.lass@gmail.com>
2022-12-10 15:15:54 -0700

    2022, day seven

---
+++ 2022/07/one.py
@@ -0,0 +1,34 @@
+import sys
+from collections import defaultdict
+
+data = open(sys.argv[1], 'r').read().replace("$ ", "").split('\n')
+
+dir_sizes = defaultdict(int)
+cwd = "/"
+path = [cwd]
+
+for line in data:
+    tokens = line.split()
+    if not tokens or tokens[0] == "ls" or tokens[0] == "dir":
+        continue
+    elif tokens[0].isdigit():
+        dir_sizes[cwd] += int(tokens[0])
+    elif tokens[0] == "cd":
+        if tokens[1] == "..":
+            subdir_size = dir_sizes.get(cwd)
+            path.pop()
+            cwd = ''.join(path)
+            dir_sizes[cwd] += subdir_size
+        elif tokens[1] == '/':
+            cwd = '/'
+        else:
+            path.append(tokens[1] + '/')
+            cwd = ''.join(path)
+
+for _ in range(len(path) - 1):
+    subdir_size = dir_sizes.get(cwd)
+    path.pop()
+    cwd = ''.join(path)
+    dir_sizes[cwd] += subdir_size
+
+print(sum(dir for dir in dir_sizes.values() if dir <= 100000))
