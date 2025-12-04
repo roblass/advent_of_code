906a30860ee702d3dbe5f871a0dc0bab7cf8c168
Rob Lass <rob.lass@gmail.com>
2025-12-04 17:03:26 -0500

    day 1

---
+++ 2025/01/one.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+dial = 50
+num_zeros = 0
+with open(sys.argv[1], 'r', encoding="utf-8") as data:
+    combo = []
+    for line in data:
+        if line[0] == "R":
+            dial = (dial + int(line[1:])) % 100
+        elif line[0] == "L":
+            dial = (dial - int(line[1:])) % 100
+
+        if dial == 0:
+            num_zeros += 1
+
+print(num_zeros)
---
+++ 2025/01/two.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+
+dial = 50
+num_zeros = 0
+with open(sys.argv[1], 'r', encoding="utf8") as data:
+    combo = []
+    for line in data:
+        rot = int(line[1:])
+        if line[0] == "R":
+            num_zeros += (dial + rot) // 100
+            dial = (dial + rot) % 100
+        elif line[0] == "L":
+            num_zeros += (((100 - dial) % 100) + rot) / 100
+            dial = (dial - rot) % 100
+
+print(num_zeros)
