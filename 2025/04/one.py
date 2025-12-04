3ede4eb4586235c59bd148feaa6d42d9c707d20a
Rob <rob.lass@gmail.com>
2025-12-04 17:33:01 -0500

    day 4

---
+++ 2025/04/one.py
@@ -0,0 +1,49 @@
+import sys
+
+
+def get_num_neighbors(mapp, yi, xi):
+    num_neighbors = 0
+    for y, x in (
+                 (yi - 1, xi),
+                 (yi, xi - 1),
+                 (yi - 1, xi - 1),
+                 (yi + 1, xi),
+                 (yi, xi + 1),
+                 (yi + 1, xi + 1),
+                 (yi - 1, xi + 1),
+                 (yi + 1, xi - 1),
+                 ):
+        if y < 0 or x < 0 or y >= len(mapp) or x >= len(mapp[y]):
+            continue
+        if mapp[y][x] == "@":
+            num_neighbors += 1
+    return num_neighbors
+
+# map is a keyword, so spell it wrong to avoid issues
+mapp = []
+with open(sys.argv[1], 'r', encoding="utf-8") as data:
+    for line in data:
+        mapp.append(list(line[:-1]))
+
+for line in mapp:
+    print(''.join(line))
+print('\n')
+
+num_accessible = 0
+result_mapp = []
+for y, line in enumerate(mapp):
+    result_mapp.append([])
+    for x, elem in enumerate(line):
+        if elem != "@":
+            result_mapp[y].append(".")
+            continue
+        num_neighbors = get_num_neighbors(mapp, y, x)
+        if num_neighbors < 4:
+            result_mapp[y].append("x")
+            num_accessible += 1
+        else:
+            result_mapp[y].append("@")
+
+for line in result_mapp:
+    print(''.join(line))
+print('\n',  num_accessible)
