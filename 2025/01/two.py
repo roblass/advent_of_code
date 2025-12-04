1bea2179436edb1882b39a425d044dca975298bc
Rob Lass <rob.lass@gmail.com>
2025-12-04 17:07:41 -0500

    typos

--- 2025/01/two.py
+++ 2025/01/two.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
 
 
 dial = 50
 num_zeros = 0
-with open(sys.argv[1], 'r', encoding="utf8") as data:
+with open(sys.argv[1], 'r', encoding="utf-8") as data:
     combo = []
     for line in data:
         rot = int(line[1:])
@@ -13,7 +12,7 @@ with open(sys.argv[1], 'r', encoding="utf8") as data:
             num_zeros += (dial + rot) // 100
             dial = (dial + rot) % 100
         elif line[0] == "L":
-            num_zeros += (((100 - dial) % 100) + rot) / 100
+            num_zeros += (((100 - dial) % 100) + rot) // 100
             dial = (dial - rot) % 100
 
 print(num_zeros)
